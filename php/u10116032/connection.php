6738d555c0f89acddbf3f2a2f7d90ed22effa89d
u10116032 <u10116032@gmail.com>
2016-07-18 23:03:30 +0800

    update server account/password
    update server account/password

--- php/u10116032/connection.php
+++ php/u10116032/connection.php
@@ -1,8 +1,8 @@
 <?php
 	
 	define ('hostname','localhost');
-	define ('user','u10116032');
-	define('password', 'u10116032');
+	define ('user','your_account'); // server account
+	define('password', 'your_password');// server password
 	define('databaseName', 'u10116032');
 
 	$connect = mysqli_connect(hostname,user,password,databaseName);
